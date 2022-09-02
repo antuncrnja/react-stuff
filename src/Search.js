ab4b21a4fca94e2d264c2fb10b60b8bedf8ec3d6
antuncrnja <antuncrnja@gmail.com>
2022-09-02 16:26:19 +0200

    filter update

--- src/Search.js
+++ src/Search.js
@@ -6,9 +6,9 @@ const Search = () => {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 
-
 	const [searchParams, setSearchParams] = useSearchParams()
 	const [query, setQuery] = useState(searchParams.get('q') ? searchParams.get('q') : '')
+	const [select, setSelect] = useState(searchParams.get('f') ? searchParams.get('f') : '')
 
 	useEffect( ()=>{
 		setLoading(true)
@@ -19,25 +19,49 @@ const Search = () => {
 		})
 		.then(data=>{
 			setLoading(false)
-			setData(data.filter(post => post.title.rendered.toLowerCase().includes(query.toString().toLowerCase()) ) )
+			setData(data.filter(post => (
+				post.title.rendered.toLowerCase().includes(query.toString().toLowerCase()) 
+				&& post.title.rendered.toLowerCase().includes(select.toString().toLowerCase()) 
+				)) )
 			setError(false)
 		})
 		.catch(err => {
 			setError(true)
 			console.log(err)
 		})
-	},[query])
+	},[query, select])
 
 	const handleSearch = (e) => {
 		const newQuery = e.target.value;
 		setQuery(newQuery)
+		searchParams.set('q', newQuery)
 
-		newQuery.length > 0 ? setSearchParams({q: newQuery}) : setSearchParams()
+		setSearchParams(searchParams)
+		if(newQuery.length == 0) {
+			searchParams.delete('q')
+			setSearchParams(searchParams)
+		}
+	}
+
+	const handleSelect = (e) => {
+		const newQuery = e.target.value;
+		setSelect(newQuery)
+		searchParams.set('f', newQuery)
+
+		setSearchParams(searchParams)
 	}
 
   return (
 	<div>
+		<h1>Search/Filter with URL Params</h1>
 		<input type="search" placeholder="search" value={query} onChange={handleSearch}/>
+
+		<select name="select" value={select} onChange={handleSelect}>
+			<option value="d">D</option>
+			<option value="devet">Devet</option>
+			<option value="berlinu">Berlinu</option>
+		</select>
+
 		{loading && <h1>Loading...</h1>}
 		
 		{data.length <= 0 && <p>There are no posts that contains: {query}</p>}
